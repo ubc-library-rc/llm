20b552156fb12895bd3f60e180d5175a4cf7a31d
aishaeldeeb <aisha.eldeeb32@gmail.com>
2024-08-20 17:00:44 -0700

    disable animation

--- slides/highlight.js
+++ slides/highlight.js
@@ -1,37 +1,37 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-// Function to show highlight
-function showHighlight(id) {
-    var element = document.getElementById(id);
-    if (element) {
-    element.classList.add('show');
-    }
-}
+// document.addEventListener('DOMContentLoaded', function() {
+// // Function to show highlight
+// function showHighlight(id) {
+//     var element = document.getElementById(id);
+//     if (element) {
+//     element.classList.add('show');
+//     }
+// }
 
-// Function to hide highlight
-function hideHighlight(id) {
-    var element = document.getElementById(id);
-    if (element) {
-    element.classList.remove('show');
-    }
-}
+// // Function to hide highlight
+// function hideHighlight(id) {
+//     var element = document.getElementById(id);
+//     if (element) {
+//     element.classList.remove('show');
+//     }
+// }
 
-// Example of showing highlights on page load
-showHighlight('highlight-square-1');
-showHighlight('highlight-square-2');
+// // Example of showing highlights on page load
+// showHighlight('highlight-square-1');
+// showHighlight('highlight-square-2');
 
-// You can also set up event listeners or other interactions
-// For example, toggle highlight visibility with a button
-document.getElementById('toggle-highlights').addEventListener('click', function() {
-    var highlight1 = document.getElementById('highlight-square-1');
-    var highlight2 = document.getElementById('highlight-square-2');
+// // You can also set up event listeners or other interactions
+// // For example, toggle highlight visibility with a button
+// document.getElementById('toggle-highlights').addEventListener('click', function() {
+//     var highlight1 = document.getElementById('highlight-square-1');
+//     var highlight2 = document.getElementById('highlight-square-2');
 
-    if (highlight1.classList.contains('show')) {
-    hideHighlight('highlight-square-1');
-    hideHighlight('highlight-square-2');
-    } else {
-    showHighlight('highlight-square-1');
-    showHighlight('highlight-square-2');
-    }
-});
-});
+//     if (highlight1.classList.contains('show')) {
+//     hideHighlight('highlight-square-1');
+//     hideHighlight('highlight-square-2');
+//     } else {
+//     showHighlight('highlight-square-1');
+//     showHighlight('highlight-square-2');
+//     }
+// });
+// });
